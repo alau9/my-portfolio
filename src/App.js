5134ac9b5f1859740d1eba67379a4baf001e41e7
Anson <ansonlau39@gmail.com>
2022-04-21 13:38:54 -0700

    added new background

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
-import React, {useEffect, Component } from "react";
-import Homepage from './pages/homepage/homepage.component';
-import './App.css';
-import ContactInfo from './components/contact/contact-info.component';
-import { Route, Routes } from "react-router-dom"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faPortrait, faLaptop, faFolder, faHome } from '@fortawesome/free-solid-svg-icons'
-import ProjectPage from './pages/projects/projects.component';
-import AboutPage from './pages/about/about.component';
-import ReactGA from 'react-ga';
-library.add(faPortrait, fab, faLaptop, faFolder, faHome)
+import React, { useEffect, Component } from "react";
+import Homepage from "./pages/homepage/homepage.component";
+import Background from "./components/background/background.component";
+
+import "./App.css";
+import ContactInfo from "./components/contact/contact-info.component";
+import { Route, Routes } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faPortrait,
+  faLaptop,
+  faFolder,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
+import ProjectPage from "./pages/projects/projects.component";
+import AboutPage from "./pages/about/about.component";
+import ReactGA from "react-ga";
+library.add(faPortrait, fab, faLaptop, faFolder, faHome);
 
 function App() {
-
   useEffect(() => {
-    ReactGA.initialize('UA-223100036-1');
+    ReactGA.initialize("UA-223100036-1");
 
-    //to report page view        
-    ReactGA.pageview(window.location.pathname)                          
+    //to report page view
+    ReactGA.pageview(window.location.pathname);
   }, []);
 
-    return (
+  return (
+    <div>
       <div>
-      <Routes>
-        <Route exact path='/' element={<Homepage/>} />
-        <Route exact path='/projects' element={<ProjectPage/>} />
-        <Route exact path='/about' element={<AboutPage/>} />
-      </Routes>
-      <ContactInfo />
+      <Background className="background"/>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/projects" element={<ProjectPage />} />
+          <Route exact path="/about" element={<AboutPage />} />
+        </Routes>
+        <ContactInfo />
+      </div>
     </div>
-    );
+  );
 }
 
 export default App;
---
+++ src/components/background/background.component.jsx
@@ -0,0 +1,50 @@
+import "./background.styles.scss";
+
+const Background = () => {
+  return (
+    <div className="stars">
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+      <div className="star"></div>
+    </div>
+  );
+};
+
+export default Background
